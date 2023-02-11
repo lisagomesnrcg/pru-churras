24a22dd680b96a9159feef7b292f9db857f37811
lisagomesnrcg <naiaracarolinegomes@gmail.com>
2023-02-10 22:28:34 -0300

    Cara ou Coroa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  moeda= 'assets/logo.png';
+  cara= 'assets/cara.png';
+  caroa= 'assets/coroa.png';
+  info= 'Clique em jogar a moeda!';
 
   constructor() {}
 
+  jogarmoeda (){
+    if(Math.random() <0.5){
+      this.moeda =this.cara;
+     this.info ="cara"
+    }else{
+      this.info ="coroa"
+      this.moeda =this.caroa;
+    }
+  }
 }
