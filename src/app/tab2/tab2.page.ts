37203895e68914e8cb343f7feeffad3f46801c42
lisagomesnrcg <naiaracgomes@gmail.com>
2023-03-03 22:27:00 -0300

    ;;;

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,52 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  valendo = 1;
 
-}
+  pontosTime1 = 0;
+  pontosTIme2 = 0;
+
+  partidasTime1 = 0;
+  partidasTim2 = 0;
+   
+  constructor(
+    private alertController: AlertController,
+    private toasttController: ToastController  
+  ) {}
+
+  pontuacaoAtual(pontos: number) {
+    this.valendo = pontos;
+
+    if (pontos == 3) {
+      this.presentToast('TRUCO!');
+    } else if (pontos == 6) {
+      this.presentToast('SEIS!');
+    } else if (pontos == 9) {
+      this.presentToast('NOVE!');
+    } else if (pontos == 12) {
+      this.presentToast('DOZE!');
+    }
+  }
+
+  pontuacaoTime: number , operacao: string) {
+    if (time == 1) {
+      if (operacao == '+') {
+        this.pontosTime1 += this.valendo;
+        this.pontosTime1 = this.pontosTime1 > 12 ? 12 : this.pontosTime1;
+      } else {
+        this.pontosTime1 -= this.valendo;
+        this.pontosTime1 = this.pontosTime1 < 0 ? 0 : this.pontosTime1;
+      }
+    } else {
+      if (operacao == '+') {
+        this.pontosTime2 += this.valendo;
+        this.pontosTime2 = this.pontosTime2 > 12 ? 12 : this.pontosTime2;
+      } else {
+        this.pontosTime2 -= this.valendo;
+        this.pontosTime2 = this.pontosTime2 < 0 ? 0 : this.pontosTime2;
+      }
+  } 
+
+  if (this.pontosTime1 == 12 || this .pontosTime2 == 12) {
+    this.finalizarPartida(); 
+     
