5dbe86f1a1eddd5a7187718609f3dc8a6cd29784
lisagomesnrcg <105024004+lisagomesnrcg@users.noreply.github.com>
2023-03-17 22:18:18 -0300

    Dado

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -6,7 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  tamanho = 6;
+  valorAtual = 0;
+  imgsDados = [
+    'assets/dice-red.png',//Posição 0
+    'assets/dice.gif'//Posição 1
+  ]
+imagemDado = this.imgsDados[0];
 
   constructor() {}
+
+  jogarDado(){
+    this.valorAtual= Math.floor( Math.random()* this.tamanho) + 1;
+  }
 
 }
